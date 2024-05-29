bb83f4cf720f5aa130b63019b4d74ece1fb5ee99
DanielaMoraDevJourney <danielamoradev@outlook.com>
2024-05-29 13:08:27 -0500

    Actualización del método en DMAboutPage.xaml.cs

--- DMNotes/DMViews/DMAboutPage.xaml.cs
+++ DMNotes/DMViews/DMAboutPage.xaml.cs
@@ -6,10 +6,12 @@ public partial class AboutPage : ContentPage
     {
         InitializeComponent();
     }
-
     private async void LearnMore_Clicked(object sender, EventArgs e)
     {
-        // Navigate to the specified URL in the system browser.
-        await Launcher.Default.OpenAsync("https://aka.ms/maui");
+        if (BindingContext is DMModels.DMAbout about)
+        {
+            // Navigate to the specified URL in the system browser.
+            await Launcher.Default.OpenAsync(about.MoreInfoUrl);
+        }
     }
 }
