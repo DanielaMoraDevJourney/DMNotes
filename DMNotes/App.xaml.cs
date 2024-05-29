b1dd6809b1fdea08ac29d0565c1f3660b0c1b112
DanielaMoraDevJourney <danielamoradev@outlook.com>
2024-05-29 12:02:28 -0500

    Agregar archivos de proyecto.

---
+++ DMNotes/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace DMNotes
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
